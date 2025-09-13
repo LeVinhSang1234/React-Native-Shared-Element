96d252be0110dda8ab20a87822b9f47292b94c86
SangLv <lsang2884@gmail.com>
2025-09-14 02:08:14 +0700

    change

--- screens/Detai.tsx
+++ screens/Detai.tsx
@@ -4,15 +4,12 @@ import ShareView from '../packages/ShareView';
 export default function Detail() {
   return (
     <View style={{ paddingTop: 100 }}>
-      <ShareView
-        shareTagElement="Video111"
-        style={{ backgroundColor: 'red', height: 200 }}
-      >
+      <ShareView shareTagElement="Video111" style={{ backgroundColor: 'red' }}>
         <Text>Hello</Text>
         <Text>Hello</Text>
         <Image
           source={require('./test.png')}
-          style={{ width: 300, height: 100 }}
+          style={{ width: '100%', height: 200 }}
         />
       </ShareView>
     </View>
